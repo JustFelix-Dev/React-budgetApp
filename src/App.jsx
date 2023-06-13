47c3b2df5164c884f91059ae83057edcfe185048
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-13 10:30:16 +0100

    Configured as a PWA

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState ,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import {
@@ -35,6 +35,21 @@ const router = createBrowserRouter(
 )
 
 function App() {
+  
+  useEffect(()=>{
+    if('serviceWorker' in navigator){
+      window.addEventListener('load',()=>{
+        navigator.serviceWorker.register('/sw.js')
+        .then((registration)=>{
+           console.log('SW registered: ' , registration);
+        })
+        .catch(registrationError =>{
+          console.log('SW registration failed: ' , registrationError);
+        })
+      })
+
+    }
+  },[])
 
   return (
     <>
